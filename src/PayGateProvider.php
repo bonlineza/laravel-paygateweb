93b9ff55069a1c6b2f5298ef7475e16d4db70051
Shaun van Tonder <shaunmichaelvantonder@gmail.com>
2018-06-22 12:16:33 +0200

    Some stylistic fixes to PayGateProvider and PayGateWeb

--- src/PayGateProvider.php
+++ src/PayGateProvider.php
@@ -1,39 +1,41 @@
 <?php
 
-    namespace MisterBrownRSA\PayGateWeb;
+namespace MisterBrownRSA\PayGateWeb;
 
-    use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
-    class PayGateProvider extends ServiceProvider
+class PayGateProvider extends ServiceProvider
+{
+    protected $defer = TRUE;
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
     {
-        protected $defer = TRUE;
-
-        /**
-         * Bootstrap the application services.
-         *
-         * @return void
-         */
-        public function boot()
-        {
-            $this->publishes([
-                __DIR__ . "/config/paygate.php" => config_path('paygate.php'),
-            ]);
-        }
-
-        /**
-         * Register the application services.
-         *
-         * @return void
-         */
-        public function register()
-        {
-            $this->app->singleton(PayGateWeb::class, function ($app) {
-                return new PayGateWeb();
-            });
-        }
-
-        public function provides()
-        {
-            return [PayGateWeb::class];
-        }
+        $this->publishes([
+            __DIR__ . "/config/paygate.php" => config_path('paygate.php'),
+        ]);
     }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(PayGateWeb::class, function ($app) {
+            return new PayGateWeb();
+        });
+    }
+
+    public function provides()
+    {
+        return [
+            PayGateWeb::class
+        ];
+    }
+}
